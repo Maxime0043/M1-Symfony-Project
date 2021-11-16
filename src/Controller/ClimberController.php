43897c1117879b11fc01d51212dd4d5a82b8d33a
Maxime0043 <max.pinna@hotmail.fr>
2021-11-16 15:44:34 +0100

    add-login-register-forms-templates

---
+++ src/Controller/ClimberController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClimberController extends AbstractController
+{
+  #[Route('/login', name: 'climber.login')]
+
+  public function login(): Response {
+     return $this->render('climber/login.html.twig');
+  }
+  
+  #[Route('/register', name: 'climber.register')]
+
+  public function register(): Response {
+     return $this->render('climber/register.html.twig');
+  }
+}
