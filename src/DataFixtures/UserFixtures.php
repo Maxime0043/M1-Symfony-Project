2eee5da1be22c5f33798e2d9f1d9d995d6d1894f
Maxime0043 <max.pinna@hotmail.fr>
2021-11-18 09:45:48 +0100

    add-user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User;
+        $user
+            ->setEmail("leegrimpeur@test.com")
+            ->setPassword('$2y$10$wyVV2ExjB8EcdJ6Yac2EJOKPQ1sqFP5AU5tift/XtDYk7EW8yYDye4') //lamachine
+            ->setRoles(['ROLE_USER'])
+        ;
+
+        $manager->persist($user);
+
+        $user = new User;
+        $user
+            ->setEmail("jeanpaul@test.com")
+            ->setPassword('$2y$10$wyVV2ExjB8EcdJ6Yac2EJOKPQ1sqFP5AU5tift/XtDYk7EW8yYDye4') //lamachine
+            ->setRoles(['ROLE_USER'])
+        ;
+
+        $manager->persist($user);
+
+        $user = new User;
+        $user
+            ->setEmail("martingeorge@test.com")
+            ->setPassword('$2y$10$wyVV2ExjB8EcdJ6Yac2EJOKPQ1sqFP5AU5tift/XtDYk7EW8yYDye4') //lamachine
+            ->setRoles(['ROLE_USER'])
+        ;
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
