20ad01e55481c81daa108f3685fea646d5bb4614
Maxime0043 <max.pinna@hotmail.fr>
2021-11-19 11:14:54 +0100

    add-climber-participation

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class CommentType extends AbstractType
 {
@@ -19,7 +20,14 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextareaType::class, array('attr' => array('rows' => '7')));
+            ->add('content', TextareaType::class, [
+                'attr'          => array('rows' => '7'),
+                'constraints'   => [
+                    new NotBlank([
+                        'message'   => 'Veulliez saisir le contenu du commentaire.'
+                    ])
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
